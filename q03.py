90f38f6c986dbe0812555e6a1db3edbab139a86a
Jhony Menezes Reis <1242120502@acd.grupotiradentes.com>
2025-05-06 20:48:48 -0300

    v1 q

--- q03.py
+++ q03.py
@@ -1,1 +1,24 @@
-print('Hello World')
+n = 1
+m = 1
+z = 0
+
+while n > 0 and m > 0:
+    n = int(input('Insira o primeiro valor do par: '))
+    m = int(input('Insira o segundo valor do par: '))
+    if n or m > 0:
+        if n > m and m > 0:
+            while n > m:
+                print(m)
+                z = z + m
+                m += 1
+            if z > 0:
+                print(f'A soma dos números é {z}')
+        if n < m and n > 0:
+            while m > n:
+                print(n)
+                z = z + m
+                n += 1
+            if z > 0:
+                print(f'A soma dos números é {z}')
+
+
