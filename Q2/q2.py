0351865a38ddc4886c30c7d2e96947b2bb2b1137
João <109487647+s1ntos@users.noreply.github.com>
2025-05-06 21:25:25 -0300

    Update q2.py

--- Q2/q2.py
+++ Q2/q2.py
@@ -1,1 +1,30 @@
-print(input('Seu animal é vertebrado ou invertebrado?'))
+def identificar_animal(palavra1, palavra2, palavra3):
+    if palavra1 == "vertebrado":
+        if palavra2 == "ave":
+            if palavra3 == "carnivoro":
+                return "aguia"
+            elif palavra3 == "onivoro":
+                return "pomba"
+        elif palavra2 == "mamifero":
+            if palvra3 == "onivoro":
+                return "homem"
+            elif palavra3 == "herbivoro":
+                return "vaca"
+    elif palavra1 == "invertebrado":
+        if palavra2 == "inseto":
+            if palavra3 == "hematofago":
+                return "pulga"
+            elif palara3 == "herbivoro":
+                return "lagarta"
+        elif palavra2 == "anelideo":
+            if palavra3 =="hematofago":
+                return "sanguessuga"
+            elif palavra3 == "onivoro":
+                return "Minoca"
+
+entrada1 = input().strip()
+entrada2= input().strip()
+entrada3 =input().strip()
+
+animal = identificar_animal(entrada1, entrada2, entrada3)
+print(animal + "aaa")
